6d2dceb503f4e6a4566d06d136b1054af1d75609
anasbehhari <an.behhari@gmail.com>
2021-04-28 18:10:06 +0000

    update

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper('.second-swiper-container',{
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+var swiper = new Swiper('.first-swiper-container',{
+    slidesPerView: 1.1,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
